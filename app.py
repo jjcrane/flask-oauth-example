f4e921ebb5c559c6f9c2372d8e1a3e5cb1f1c791
jjcrane <jjcrane@gmail.com>
2023-11-03 20:19:21 -0400

    test

--- app.py
+++ app.py
@@ -108,7 +108,8 @@ def token_required(f):
         try:
            # decode the token to obtain user public_id
             data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
-            current_user = User.query.filter_by(public_id=data['public_id']).first()
+            print(data)
+            #current_user = User.query.filter_by(public_id=data['public_id']).first()
         except:
             return make_response(jsonify({"message": "Invalid token!"}), 401)
          # Return the user information attached to the token
